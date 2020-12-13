fe8a9b32fa6158c176a77853f0410bbde1be5b6c
unknown <abdokiller05@gmail.com>
2020-12-13 17:38:55 +0100

    13/12/2020

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request
 import pandas as pd
 import requests
-
+import http.client
+import mimetypes
+import json 
 def list_data(per1, per2):
 	temp = []
 	data = pd.read_json('https://{}.openfoodfacts.org/{}.json'.format(per1, per2))
@@ -13,7 +15,7 @@ def list_data(per1, per2):
 	 
 countries = []
 
-
+#(nutrient_levels)(ingredients_text_with_allergens)(ingredients_text)(image_front_small_url)
 
 countries_data = pd.read_json('https://world.openfoodfacts.org/data/taxonomies/countries.json')
 
@@ -27,9 +29,26 @@ app = Flask(__name__)
 @app.route('/index', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        countrie = 'en:'+request.form['Countries']
-        pay = countries_data[str(countrie)]['country_code_3']
-        return redirect(url_for('search', pays = pay['en']))
+    	countrie = 'en:'+request.form['Countries']
+    	key_world = request.form['search']
+    	pay = countries_data[str(countrie)]['country_code_3']
+    	url = "https://"+pay['en']+".openfoodfacts.org/cgi/search.pl?search_terms="+key_world+"&search_simple=1&action=process&json=1"
+    	#url = "https://{}.openfoodfacts.org/cgi/search.pl?search_terms={}&user_id=abdotalib&password=abd123do&search_simple=1&jqm=1".format(pays, key_world)
+    	payload = {}
+    	headers= {}
+    	response = requests.request("GET", url, headers=headers, data = payload, verify=False)
+    	data =  response.text.encode('utf8')#pd.read_json()
+    	'''
+    	conn = http.client.HTTPSConnection("{}.openfoodfacts.org".format(pays))
+    	payload = ''
+    	headers = {}
+    	conn.request("GET", "/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=breakfast_cereals&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1=A&additives=without&ingredients_from_palm_oil=without&json=true", payload, headers)
+    	res = conn.getresponse()
+    	data = res.read()
+    	#print(data.decode("utf-8"))
+    	jsonfiles = json.loads(data.to_json(orient='records'))
+    	'''
+    	return  render_template("all_info.html", d = data)
     return render_template("index.html", Countries = countries, Countries_len = len(countries))
 
 #, Categories = categories, Categories_len = len(categories), Allergens = allergens, Allergens_len = len(allergens)
@@ -37,12 +56,22 @@ def index():
 def search(pays):
 	if request.method == 'POST':
 		key_world = request.form['search']
-		url = "https://"+str(pays)+".openfoodfacts.org/cgi/search.pl?search_terms="+str(key_world)+"&search_simple=1&action=process&json=1"
+		url = "https://world.openfoodfacts.org/cgi/search.pl?search_terms="+key_world+"&search_simple=1&action=process&json=1"
+		#url = "https://{}.openfoodfacts.org/cgi/search.pl?search_terms={}&user_id=abdotalib&password=abd123do&search_simple=1&jqm=1".format(pays, key_world)
+
 		payload = {}
 		headers= {}
-		response = requests.request("GET", url, headers=headers, data = payload)
+		response = requests.request("GET", url, headers=headers, data = payload, verify=False)
 		data =  response.text.encode('utf8')#pd.read_json()
-		return redirect(url_for('data_aff', data = data))	
+		'''
+		conn = http.client.HTTPSConnection("{}.openfoodfacts.org".format(pays))
+		payload = ''
+		headers = {}
+		conn.request("GET", "/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=breakfast_cereals&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1=A&additives=without&ingredients_from_palm_oil=without&json=true", payload, headers)
+		res = conn.getresponse()
+		data = res.read()
+		#print(data.decode("utf-8"))'''
+		return  render_template("all_info.html", d = data)	
 	return render_template("search.html")
 	'''brands = list_data(pays, "brands")
 	categories = list_data(pays, "categories")
@@ -65,4 +94,5 @@ def data_aff(data):
 
 if __name__ == '__main__':
    app.run(debug = True)
+
 
